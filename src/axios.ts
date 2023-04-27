42fab457e9df1adcdb79a892dd86c86f8e053cb5
KyungTae Park <96967183+badmaniacs@users.noreply.github.com>
2023-04-27 20:18:25 +0900

    ✨ Feat :  axios module

---
+++ src/axios.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosInstance } from 'axios';
+
+class IidxApi {
+  private client: AxiosInstance;
+  constructor() {
+    this.client = axios.create({
+      baseURL: 'https://p.eagate.573.jp/game/2dx/30/',
+    });
+  }
+
+  getStatus() {
+    return this.client.get('/djdata/status.html');
+  }
+
+  getScore(level:number,offset:number) {
+    return this.client.get(
+      `/djdata/music/difficulty.html?difficult=${level}&style=0&disp=1&offset=${offset}`
+    );
+  }
+}
+
+export const iidxApi = new IidxApi();
